7b9decc6c179370172518288e2100fb9b24a5eab
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-11-04 23:57:30 +0100

    working on the javascript

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-const email = document.getElementById("email").value.trim();
-document.getElementsByClassName("email_form").addEventsListener("submit", (event) => {
-  event.preventDefault()
+const email = document.getElementById('email').value.trim()
+document
+  .getElementsByClassName('email_form')
+  .addEventsListener('submit', event => {
+    event.preventDefault()
 
-  validateInput()
-})
+    validateInput()
+  })
 
 const setError = (element, message) => {
-  const errorDisplay = element.querySelector(".error");
-  errorDisplay.innerText = message;
-  element.classList.add("error");
-  element.classList.add("success");
+  const errorDisplay = element.querySelector('.error')
+  errorDisplay.innerText = message
+  element.classList.add('error')
+  element.classList.add('success')
 }
 
-const setSuccess = (element) => {
-  const errorDisplay = element.querySelector(".error")
-  errorDisplay.innerText = ""
-  element.classList.add("success")
+const setSuccess = element => {
+  const errorDisplay = element.querySelector('.error')
+  errorDisplay.innerText = ''
+  element.classList.add('success')
+}
+const isValidEmail = email => {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return re.test(String(email).toLocaleLowerCase)
 }
 
 validateInput = () => {
-  if (email === ''){
-    setError(email, "Email is required!")
-  } else{
-
+  if (email === '') {
+    setError(email, 'Email is required!')
+  } else if (!isValidEmail) {
+    setError(email, 'Provide a valid Email.')
+  } else {
+    setSuccess(email)
   }
 }
